e903824185ad0162713f04a81973c99916f5aae0
Jeeho Ahn <jeeho@umich.edu>
2024-08-10 01:31:04 +0000

    in progress

--- include/reloPush/data_collector.h
+++ include/reloPush/data_collector.h
@@ -29,6 +29,40 @@ class PathInfo
     PathInfo(std::string vertex_name, moveType path_type, State& from_pose, State& to_pose, std::vector<State>& path_in) 
     : vertexName(vertex_name), type(path_type), fromPose(from_pose), toPose(to_pose), path(path_in)
     {}
+
+    bool is_pushing()
+    {
+        if(type == moveType::none)
+            return false;
+        else
+            return true;
+    }
+
+    // Sum of euclidean distances between consecutive waypoints
+    float pathLength() 
+    {
+        float totalLength = 0.0;
+
+        for (size_t i = 1; i < path.size(); ++i) {
+            float dx = path[i].x - path[i-1].x;
+            float dy = path[i].y - path[i-1].y;
+            totalLength += std::sqrt(dx * dx + dy * dy);
+        }
+
+        return totalLength;
+    }
+    
+};
+
+class PathInfoList
+{
+    public:
+    std::vector<PathInfo> paths;
+
+    PathInfoList()
+    {paths.clear();}
+
+    
 };
 
 class DataCollector
