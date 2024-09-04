14f84bb68fffa670512f3cadce3f3755c77f2e57
Jeeho Ahn <jeeho@umich.edu>
2024-09-04 07:52:50 -0400

    cleared out-dated logs

--- script/repeat_partial.sh
+++ script/repeat_partial.sh
@@ -2,6 +2,9 @@
 
 
 
-./repeat.sh -t exp_6o_var_robot.txt
-./repeat_mp_only.sh -t exp_6o_var_robot.txt
+./repeat.sh -t exp_2o_var_robot.txt
+./repeat_mp_only.sh -t exp_2o_var_robot.txt
+
+./repeat.sh -t exp_3o_var_robot.txt
+./repeat_mp_only.sh -t exp_3o_var_robot.txt
 
