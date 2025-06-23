ab45df5ef780d5d8737758ad1efbeef0921945d6
Jeeho Ahn <jeeho@umich.edu>
2025-06-23 14:24:30 -0400

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     std::string filename = "iros_obj6.txt";
 
     int instance_ind = 0;
-    bool use_opt = false;
+    bool use_opt = true;
     bool vis = true;
     //bool sim = true;
     planningSimOrReal sim = planningSimOrReal::planOnly;
